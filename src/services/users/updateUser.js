e430e6a84d963d433ecc87fa5906aff9bad597ad
markflerko <mark.flerko@satelliteinnovations.io>
2021-12-10 22:14:05 +0300

    feat: update put method

--- src/services/users/updateUser.js
+++ src/services/users/updateUser.js
@@ -1,7 +1,13 @@
 const { database } = require('../../repository/database');
 
 const updatePerson = ({ id, body }) => {
-  database[id] = { ...database[id], ...body };
+  database[id] = {
+    id: database[id].id,
+    name: body.name ? body.name : database[id].name,
+    login: body.login ? body.login : database[id].login,
+    password: body.password ? body.password : database[id].password,
+  };
+  console.log(database[id]);
   return database[id];
 };
 
