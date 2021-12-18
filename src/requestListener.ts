0462b4c366f350ffbc8986cb2fdffd54c296af93
markflerko <markflerko@gmail.com>
2021-12-19 01:53:55 +0300

    feat: add req res types

--- src/requestListener.ts
+++ src/requestListener.ts
@@ -1,33 +1,35 @@
+import { IncomingMessage, ServerResponse } from 'http';
+
 const responseBuilder = require('./utils/responseBuilder');
 const emitter = require('./utils/eventEmitter');
 require('./routes/users');
 require('./routes/boards');
 require('./routes/tasks');
 
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export default async (req: any, res: any) => {
+export default async (req: IncomingMessage, res: ServerResponse) => {
   try {
     const { method, url } = req;
-    const pathFull = url.split('/').slice(1);
-    const [path, pathId, pathIdPath] = pathFull;
+    if (url) {
+      const pathFull = url.split('/').slice(1);
+      const [path, pathId, pathIdPath] = pathFull;
 
-    if (pathFull.length >= 3 && pathIdPath !== 'tasks') {
-      responseBuilder({
-        res,
-        code: 404,
-        message: 'Sorry we have only one layer nest\n',
-      });
-    } else {
-      const route =
-        pathIdPath === 'tasks' ? `${path}/${pathId}/${pathIdPath}` : path;
-      const emitted = emitter.emit(`[${route}]:[${method}]`, req, res);
-      if (!emitted) {
+      if (pathFull.length >= 3 && pathIdPath !== 'tasks') {
         responseBuilder({
           res,
           code: 404,
-          message: 'no such endpoint\n',
+          message: 'Sorry we have only one layer nest\n',
         });
+      } else {
+        const route =
+          pathIdPath === 'tasks' ? `${path}/${pathId}/${pathIdPath}` : path;
+        const emitted = emitter.emit(`[${route}]:[${method}]`, req, res);
+        if (!emitted) {
+          responseBuilder({
+            res,
+            code: 404,
+            message: 'no such endpoint\n',
+          });
+        }
       }
     }
   } catch (error) {
