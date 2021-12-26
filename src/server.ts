e048c7b0f58c3cdc6b926779abf8bbf152fa4641
markflerko <markflerko@gmail.com>
2021-12-26 20:15:26 +0300

    feat: change all console.log on logger

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,5 @@
+import { logger } from './logger/index';
 import config from './common/config';
 import app from './app';
 
-app.listen(config.PORT, () =>
-  console.log(`App is running on http://localhost:${config.PORT}`)
-);
+app.listen(config.PORT, () => logger.info(`App is running on http://localhost:${config.PORT}`));
