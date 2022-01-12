d71230f5b1900f2554244f15279f5f62d6f41bc6
markflerko <markflerko@gmail.com>
2022-01-12 21:32:50 +0300

    refactor: remove aside []

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ build:
 	docker build -t app .
 
 run:
-	docker run -d -p 4000:4000 -v logs:/app/logs --rm --name app app:latest
+	docker run -d -p 4000:4000 -v logs:/app/logs -v db:/app/src/database --rm --name app app:latest
 
 stop:
 	docker stop app
--- src/repository/database.ts
+++ src/repository/database.ts
@@ -1,10 +1,7 @@
 import { ITask } from '../models/Task';
 import { IBoard } from '../models/Board';
 import { IUser } from '../models/User';
-import users from '../database/users.json';
-import boards from '../database/boards.json';
-import tasks from '../database/tasks.json';
 
-export const usersRepository: IUser[] = users;
-export const boardsRepository: IBoard[] = boards;
-export const tasksRepository: ITask[] = tasks;
+export const usersRepository: IUser[] = [];
+export const boardsRepository: IBoard[] = [];
+export const tasksRepository: ITask[] = [];
