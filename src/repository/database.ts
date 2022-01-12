db4907140914191969cff0f86ccfcbd568de01f0
markflerko <markflerko@gmail.com>
2022-01-12 20:35:20 +0300

    refactor: db -> json

--- src/repository/database.ts
+++ src/repository/database.ts
@@ -2,7 +2,9 @@ import { ITask } from '../models/Task';
 import { IBoard } from '../models/Board';
 import { IUser } from '../models/User';
 import users from '../database/users.json';
+import boards from '../database/boards.json';
+import tasks from '../database/tasks.json';
 
 export const usersRepository: IUser[] = users;
-export const boardsRepository: IBoard[] = [];
-export const tasksRepository: ITask[] = [];
+export const boardsRepository: IBoard[] = boards;
+export const tasksRepository: ITask[] = tasks;
